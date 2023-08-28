e2ab546daccbec894de60737bf077324faad6a95
Angel <scorpion8007@mail.com>
2023-08-28 11:22:52 +0300

    Linku dadarymas

--- irasai/urls.py
+++ irasai/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('', views.index, name='namu_puslapis_url'),
-               path('artistai/', views.artistai, name='artistai-visi-url')
+               path('artistai/', views.artistai, name='artistai-visi-url'),
+               path('artistai/<int:artistas_id>', views.artistas, name='artistas-vienas-url')
 
 ]
