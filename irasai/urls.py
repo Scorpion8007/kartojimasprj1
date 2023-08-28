bf77c26ec9f3d807053e281d09e8b3672e087310
Angel <scorpion8007@mail.com>
2023-08-28 11:28:51 +0300

    Linku dadarymas gyvas

--- irasai/urls.py
+++ irasai/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [path('', views.index, name='namu_puslapis_url'),
                path('artistai/', views.artistai, name='artistai-visi-url'),
-               path('artistai/<int:artistas_id>', views.artistas, name='artistas-vienas-url')
+               path('artistai/<int:artistas_id>', views.artistas, name='artistas-vienas-url'),
 
 ]
